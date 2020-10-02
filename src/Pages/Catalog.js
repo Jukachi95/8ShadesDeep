dde815b0c25a134f6c5f8923b3f03a9fa10b8fc1
Joel Okoro <jukachi95@gmail.com>
2020-10-02 09:39:28 +0100

    Implementing props in Catalog.js for the description

--- src/Pages/Catalog.js
+++ src/Pages/Catalog.js
@@ -7,10 +7,19 @@ const Catalog = () =>{
     return(
         <div>
             <h1 style = {{color: 'black'}}>Our Collaborations</h1>
-            <CollabContainer title = {'We Are Cow'} image = {require('../img/WeAreCow.jpg')} className = {'collab__container'}/>
+            <CollabContainer title = {'We Are Cow'} 
+            image = {require('../img/WeAreCow.jpg')} 
+            className = {'collab__container'}
+            compDescription = {''}
+            />
 
             
-            <CollabContainer title = {'The JOINT'} image= {require('../img/TheJoint.jpg')} className = {'collab__container__reverse'}/>
+            <CollabContainer title = {'The JOINT'} 
+            image= {require('../img/TheJoint.jpg')} 
+            className = {'collab__container__reverse'}
+            compDescription = {'Exotic Candy & Snacks Store'}
+            firstAddress = {'props.firstAddress'}
+            />
         </div>
     )
 }
