f0691575c8d63e18c69078c8f5a583fb7b9ee54e
Joel Okoro <jukachi95@gmail.com>
2020-10-02 09:18:02 +0100

    Props for description of catalog flex cards

--- src/Components/CollabContainer.js
+++ src/Components/CollabContainer.js
@@ -7,12 +7,19 @@ const CollabContainer = (props) => {
         <div className = {props.className}>
             <div className = "body__left">
                 <h1>{props.title}</h1> 
+                {/* <h3> props.compDescription */}
                 <h3>Exotic Candy & Snacks Store</h3>
+                {/* <p>props.firstAddress </p> */}
                 <p>Unit 6 Premier House </p>
+                {/* props.secondAddress */}
                 <p>Rolfe Street</p>
+                {/* <h3>props.openingTimesTitle */}
                 <h3>Opening Times:</h3>
+                {/* props.openingTime */}
                 <p>12:00 - 20:00</p>
+                {/* props.contactTitle */}
                 <h3>Contact Number:</h3>
+                {/* props.contactNumber */}
                 <p>0121 448 2084</p>
                 {/* Telephone Icon */}
             </div>
