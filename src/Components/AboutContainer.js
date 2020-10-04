46919dd22d9dc54638aca77863c0fd8667ac38fb
Joel Okoro <jukachi95@gmail.com>
2020-10-04 23:44:34 +0100

    Created css file for About and formatted About page

--- src/Components/AboutContainer.js
+++ src/Components/AboutContainer.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import './About.css'
 
 const AboutContainer = () => {
 
     return(
      <div>
+        <div className = "about__main__container">
+                <div className = "about__title">
+                    <h1 style = {{color: 'black', marginTop: "50px"}}>About 8ShadesDeep</h1>
+                </div>
 
-         <div className = "about__title">
-        <h1 style = {{color: 'black', marginTop: "50px"}}>About 8ShadesDeep</h1>
-        </div>
-
-        <div className = "about__photo">
-        <img src = {require('../img/sledge_profile.jpg')}></img>
-        <h4>Sledge Tshibangu</h4>
-        <h5>Founder</h5>
-        <p>Director, Advertiser, Photographer, Music Producer</p>
-        </div>
-
+                <div className = "about__photo">
+                    <img src = {require('../img/sledge_profile.jpg')}></img>
+                    <h4>Sledge Tshibangu</h4>
+                    <h5>Founder</h5>
+                    <p>Director, Advertiser, Photographer, Music Producer</p>
+                </div>
+        </div> 
     </div>
 
     )
