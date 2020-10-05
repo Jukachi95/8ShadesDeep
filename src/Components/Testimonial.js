0143585a43c54e1cef2796d2cfbecda5253abfca
Joel Okoro <jukachi95@gmail.com>
2020-10-05 22:27:58 +0100

    Testimonial cards and css file creation

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import './Testimonial.css'
 
 const Testimonial = () => {
 
 return (
     <div>
         <div className = "testimonial__container">
-        
+            <div className = "testimonial__card">
+                    <div>
+                        <img src =  {require('../img/model1.jpg')}></img>
+                    </div>
+
+                    <div>
+                        <img src = {require('../img/model1.jpg')}></img>
+                    </div>
+
+                    <div>
+                        <img src = {require('../img/model1.jpg')}></img>
+                    </div> 
+               
+            </div>
         </div>
     </div>
 )
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../Components/Showcase.css'
 import AboutContainer from '../Components/AboutContainer'
+import Testimonial from '../Components/Testimonial';
 
 const About = () =>{
     return(
         <div>
             <AboutContainer/>
+            <Testimonial/>
         </div>
     )
 }
