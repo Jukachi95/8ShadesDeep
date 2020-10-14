562248f9e2940cf36f79ded3f9b67059e321331c
Joel Okoro <jukachi95@gmail.com>
2020-10-14 12:19:58 +0100

    Updated text and classs names for text in Founder container

--- src/Components/Founder.js
+++ src/Components/Founder.js
@@ -10,11 +10,15 @@ const Founder = () => {
 
             <div className = "founder__main__text">
                 <blockquote>
-                    <h4>The idea behind the collective was to bring a lot
-                        of ideas from various individuals and bring them all to life
+                    <h4>"The idea behind the collective was to bring a lot
+                        of ideas from various individuals and bring them all to life. We're
+                        always looking for ways to collaborate with the community and those that
+                        want to make a difference in their current field"
                     </h4>
+                    <p className = "founder__inner__name"> Sledge Tchibangu</p>
+                    <p className = "founder__inner__title">Founder</p>
                 </blockquote>
-                <p>  - Sledge Tchibangu</p>
+                
             </div>
        </div>
        
